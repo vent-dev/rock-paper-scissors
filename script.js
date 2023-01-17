a43f9b0d4bc50c78071d87738fcf118feebf2c89
vent-dev <55215656+vent-dev@users.noreply.github.com>
2023-01-17 20:59:57 +0000

    Temporarily removed winner check, cleaned up code and added some colours

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let choices = ['rock', 'paper', 'scissors']
-let resultMessage = '';
-let playerScore = 4;
-let computerScore = 0;
+let [playerScore, computerScore] = [0, 0];
 
 const playerSelection = document.querySelectorAll('.button');
+const result = document.querySelector('.result');
 const contentWindow = document.querySelector('.contentWindow');
 
 playerSelection.forEach(selection => {
@@ -15,77 +14,53 @@ playerSelection.forEach(selection => {
 function playRound(playerChoice){
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-    const result = document.createElement('p');
-
-    gameRound(computerChoice, playerChoice);
-
-    result.textContent = resultMessage;
-
+    result.textContent = gameRound(computerChoice, playerChoice);
     contentWindow.append(result);
-
-    return resultMessage;
-}
-
-function checkWinner(PlayerScore, computerScore){
-    if (playerScore >= 5 && computerScore >= 5){
-        resultMessage = (`It's a tie! Better look next time! Your total is ${playerScore} and your opponents total is ${computerScore}.`);
-    }
-    else if (playerScore >= 5){
-        resultMessage = (`You have won! Congratulations! Your total is ${playerScore} while your opponent fell short with a total of ${computerScore}!`);
-    }
-
-    else if (computerScore >= 5){
-        resultMessage = (`You have lost! :( Your opponents total is ${computerScore} while you fell short with a measly total of ${playerScore}.`);
-    }
-
-    else {
-        return;
-    }
 }
 
 function gameRound(computerChoice, playerChoice){
 
     if (playerChoice === computerChoice) {
-        resultMessage =  (`You both picked the same thing! No points added or taken! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`You both picked the same thing! No points added or taken! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     // win situations 
     if (computerChoice === "rock" && playerChoice === "paper"){
         playerScore ++;
         computerScore --;
-        resultMessage = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "paper" && playerChoice === "scissors"){
        playerScore ++;
         computerScore --;
-        resultMessage = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "scissors" && playerChoice === "rock"){
         playerScore ++;
         computerScore --;
-        resultMessage = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     // loss situations
     if (playerChoice === "rock" && computerChoice === "paper"){
         playerScore --;
         computerScore ++;
-        resultMessage = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "paper" && computerChoice === "scissors"){
         playerScore --;
         computerScore ++;
-        resultMessage = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "scissors" && computerChoice === "rock"){
         playerScore --;
         computerScore ++;
-        resultMessage = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        return(`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
-    checkWinner(playerScore, computerScore);
+
 }
