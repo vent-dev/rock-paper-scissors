8a6447ad95bef94eaff3e5352f51bb7a955ed272
vent-dev <55215656+vent-dev@users.noreply.github.com>
2023-01-17 16:07:12 +0000

    Added all the functionality for the ui

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let choices = ['rock', 'paper', 'scissors']
 let playerScore = 0;
+let opponentScore = 0;
 
 const btnRock = document.querySelector('#btnRock');
 const btnPaper = document.querySelector('#btnPaper');
@@ -25,46 +26,69 @@ function playRound(playerChoice){
     result.textContent = gameRound(computerChoice, playerChoice); 
 
     const total = document.createElement('p');
-    total.textContent = `Game over! Your total score was ${playerScore}`;
+    total.textContent = `Your total score is ${playerScore}`;
 
-    contentWindow.append(result, total);
+    const winner = document.createElement('p');
+    winner.textContent = checkWinner(opponentScore, playerScore);
+
+    contentWindow.append(result, total, winner);
 };
+
+function checkWinner(opponentScore, playerScore){
+    if (opponentScore >= 5){
+        return(`The opponent wins with a score of ${opponentScore} points! Sadly, you stayed at ${playerScore} points. Better luck next time!`);
+    }
+    
+    else if (playerScore >= 5) {
+        return(`You win! Great job! Your score came to ${playerScore} points, while your opponent stayed at ${opponentScore} points!`);
+    }
+
+    else {
+        return;
+    }
+}
 
 function gameRound(computerChoice, playerChoice){
 
     // win situations 
     if (computerChoice === "rock" && playerChoice === "paper"){
         playerScore += 5;
-        return(`You win! Paper beats Rock! 5 points have been added! Your total score is ${playerScore}`);
+        opponentScore -= 5;
+        return(`Paper beats Rock! 5 points have been added! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     else if (computerChoice === "paper" && playerChoice === "scissors"){
         playerScore += 5;
-        return(`You win! Scissors beats Paper! 5 points have been added! Your total score is ${playerScore}`);
+        opponentScore -= 5;
+        return(`Scissors beats Paper! 5 points have been added! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     else if (computerChoice === "scissors" && playerChoice === "rock"){
         playerScore += 5;
-        return(`You win! Rock beats Scissors! 5 points have been added! Your total score is ${playerScore}`);
+        opponentScore -= 5;
+        return(`Rock beats Scissors! 5 points have been added! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     // loss situations
     else if (playerChoice === "rock" && computerChoice === "paper"){
         playerScore -= 5;
-        return(`You lose! Paper beats Rock! 5 points have been taken! Your total score is ${playerScore}`);
+        opponentScore += 5;
+        return(`Paper beats Rock! 5 points have been taken! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     else if (playerChoice === "paper" && computerChoice === "scissors"){
         playerScore -= 5;
-        return(`You lose! Scissors beats Paper! 5 points have been taken! Your total score is ${playerScore}`);
+        opponentScore += 5;
+        return(`Scissors beats Paper! 5 points have been taken! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     else if (playerChoice === "scissors" && computerChoice === "rock"){
         playerScore -= 5;
-        return(`You lose! Rock beats Scissors! 5 points have been taken! Your total score is ${playerScore}`);
+        opponentScore += 5;
+        return(`Rock beats Scissors! 5 points have been taken! Your total score is ${playerScore}, opponent score is ${opponentScore}`);
     }
 
     else {
-        return (`You both picked the same thing! It's a tie! No points added or taken! Your total score is ${playerScore}`)
+        return (`You both picked the same thing! No points added or taken! Your total score is ${playerScore}, opponent score is ${opponentScore}`)
     }
 };
