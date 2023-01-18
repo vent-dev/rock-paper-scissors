b57bec4e9a371cbecb018e5cde2cb64138b1397c
vent-dev <55215656+vent-dev@users.noreply.github.com>
2023-01-18 19:21:43 +0000

    Finished basic structure of page

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let choices = ['rock', 'paper', 'scissors']
 let [playerScore, computerScore] = [0, 0];
 
+const buttons = document.querySelector('.buttons');
 const playerSelection = document.querySelectorAll('.button');
 const result = document.querySelector('.result');
 const contentWindow = document.querySelector('.contentWindow');
+const computerTotal = document.querySelector('.computerScore .window');
+const playerTotal = document.querySelector('.playerScore .window');
 
 playerSelection.forEach(selection => {
     selection.addEventListener('click', element => {
         playRound(element.target.alt);
+        element.currentTime = 0;
     })
 });
 
@@ -15,7 +19,6 @@ function playRound(playerChoice){
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
     gameRound(computerChoice, playerChoice);
-
     contentWindow.appendChild(result);
 }
 
@@ -27,57 +30,63 @@ function gameRound(computerChoice, playerChoice){
 
     // win situations 
     if (computerChoice === "rock" && playerChoice === "paper"){
-        playerScores();
+        playerScore ++;
+        playerTotal.textContent = playerScore;
         result.textContent = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "paper" && playerChoice === "scissors"){
-        playerScores();
+        playerScore ++;
+        playerTotal.textContent = playerScore;
         result.textContent = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "scissors" && playerChoice === "rock"){
-        playerScores();
+        playerScore ++;
+        playerTotal.textContent = playerScore;
         result.textContent = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     // loss situations
     if (playerChoice === "rock" && computerChoice === "paper"){
-        computerScores();
+        computerScore ++;
+        computerTotal.textContent = computerScore;
         result.textContent = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "paper" && computerChoice === "scissors"){
-        computerScores();
+        computerScore ++;
+        computerTotal.textContent = computerScore;
         result.textContent = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "scissors" && computerChoice === "rock"){
-        computerScores();
+        computerScore ++;
+        computerTotal.textContent = computerScore;
         result.textContent = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     checkWinner();
 }
 
-function playerScores(){
-    playerScore ++;
-    computerScore < 0 ? computerScore = 0 : true;
-    playerScore < 0 ? playerScore = 0 : true;
-}
-
-function computerScores(){
-    computerScore ++;
-    computerScore < 0 ? computerScore = 0 : true;
-    playerScore < 0 ? playerScore = 0 : true;
-}
-
 function checkWinner(){
     if (computerScore >= 5){
         result.textContent = (`You lose with a total score of ${playerScore} while your opponent won with a score of ${computerScore}!`);
+        restart();
     }
 
     else if (playerScore >= 5){
         result.textContent = (`You win with a total score of ${playerScore} while your opponent fell short with a score of ${computerScore}!`);
+        restart();
     }
+}
+
+function restart(){
+    playerSelection.forEach(selection => { selection.remove() });
+    const restart = document.createElement('button');
+    restart.textContent = 'restart';
+    buttons.appendChild(restart);
+    restart.addEventListener('click', () => {
+        window.location.reload();
+    });
 }
