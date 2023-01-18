b3be24b632505dc8059af3b21c9bc3dcc888a4f7
vent-dev <55215656+vent-dev@users.noreply.github.com>
2023-01-18 11:16:53 +0000

    Fixed issue with a winner not showing and min variable value at 0

--- script.js
+++ script.js
@@ -14,53 +14,70 @@ playerSelection.forEach(selection => {
 function playRound(playerChoice){
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-    result.textContent = gameRound(computerChoice, playerChoice);
-    contentWindow.append(result);
+    gameRound(computerChoice, playerChoice);
+
+    contentWindow.appendChild(result);
 }
 
 function gameRound(computerChoice, playerChoice){
 
     if (playerChoice === computerChoice) {
-        return(`You both picked the same thing! No points added or taken! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        result.textContent = (`You both picked the same thing! No points added or taken! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     // win situations 
     if (computerChoice === "rock" && playerChoice === "paper"){
-        playerScore ++;
-        computerScore --;
-        return(`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        playerScores();
+        result.textContent = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "paper" && playerChoice === "scissors"){
-       playerScore ++;
-        computerScore --;
-        return(`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        playerScores();
+        result.textContent = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (computerChoice === "scissors" && playerChoice === "rock"){
-        playerScore ++;
-        computerScore --;
-        return(`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        playerScores();
+        result.textContent = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     // loss situations
     if (playerChoice === "rock" && computerChoice === "paper"){
-        playerScore --;
-        computerScore ++;
-        return(`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        computerScores();
+        result.textContent = (`Paper beats Rock! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "paper" && computerChoice === "scissors"){
-        playerScore --;
-        computerScore ++;
-        return(`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        computerScores();
+        result.textContent = (`Scissors beats Paper! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
     else if (playerChoice === "scissors" && computerChoice === "rock"){
-        playerScore --;
-        computerScore ++;
-        return(`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
+        computerScores();
+        result.textContent = (`Rock beats Scissors! Your total score is ${playerScore}, opponent score is ${computerScore}`);
     }
 
+    checkWinner();
+}
 
+function playerScores(){
+    playerScore ++;
+    computerScore < 0 ? computerScore = 0 : true;
+    playerScore < 0 ? playerScore = 0 : true;
+}
+
+function computerScores(){
+    computerScore ++;
+    computerScore < 0 ? computerScore = 0 : true;
+    playerScore < 0 ? playerScore = 0 : true;
+}
+
+function checkWinner(){
+    if (computerScore >= 5){
+        result.textContent = (`You lose with a total score of ${playerScore} while your opponent won with a score of ${computerScore}!`);
+    }
+
+    else if (playerScore >= 5){
+        result.textContent = (`You win with a total score of ${playerScore} while your opponent fell short with a score of ${computerScore}!`);
+    }
 }
